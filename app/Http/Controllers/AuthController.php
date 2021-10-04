da91a17a8befe0df63308bca0204665875418347
ferri yusra <ferisakuragi@gmail.com>
2021-10-04 15:58:58 +0700

    remove hash in config/auth, add should use api in construct authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
+        return auth()->shouldUse('api');
     }
 
      /**
