46cdcfd4db7f4a5f8d4ae15533bb0d858bc285d0
ferri yusra <ferisakuragi@gmail.com>
2021-10-04 16:20:05 +0700

    create post data forum

---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+
+class ForumController extends Controller
+{
+    public function __construct()
+    {
+        return auth()->shouldUse('api');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make(request()->all(), [
+            'title' => 'required|min:5',
+            'body' => 'required|min:10',
+            'category' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(
+                $validator->messages()
+            );
+        }
+
+
+        try {
+            $user = auth()->userOrFail();
+        } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json([
+                'message' => 'Not authenticated, you have to login first',
+            ]);
+        }
+
+        $data = $user->forums()->create([
+                'title' => request('title'),
+                'body' => request('body'),
+                'slug' => Str::slug(request('title'), '-') . '-' . time(),
+                'category' => request('category'),
+        ]);
+
+        // apakah generate token, auto login atau hanya response berhasil
+        return response()->json([
+            'message' => 'Successfuly posted',
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
